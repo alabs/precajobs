fb6cf74f4e5cc627006380eba2c67ab27f240c6c
Andrés Pereira <apereira@alabs.es>
2011-11-28 09:17:32 -0500

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Precajobs::Application.config.secret_token = '13bda0aa6ef34aed938eedbd97813f1093884852ccf780e4b69977862f0a6b0a2e212c4debd673776c8f5036a15d5a29b5280dd16a96a9fc703b2bbfb0b76978'
