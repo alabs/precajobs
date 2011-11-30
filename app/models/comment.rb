116679696f43d57b304340c405fce8974074ec29
Andrés Pereira <apereira@alabs.es>
2011-11-30 04:46:27 -0500

    model validations for offer and comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :offer
   belongs_to :user
+
+  validates :body, :presence => true
+
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,9 +3,17 @@ class Offer < ActiveRecord::Base
 
   has_many :comments
 
+  validates :link, :title, :description, :summary, :screenshot_file_name, :presence => true
+
+  validates :link, :uniqueness => true
+
+  validates_format_of :link, :with => URI::regexp(%w(http https))  
+
   # Paperclip
   has_attached_file :screenshot,
     :styles => {
       :thumb=> "100x100#",
-      :small  => "150x150>" }
+      :small  => "150x150>" 
+  }
+
 end
