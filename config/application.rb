ba772b0772e983401699e6d48f9bcd4cf044899e
Andrés Pereira <apereira@alabs.es>
2011-12-03 04:15:02 -0500

    rails 3.1.1 upgrade

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,15 @@ module Precajobs
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+     
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
+     
+    # Change the path that assets are served from
+    # config.assets.prefix = "/assets"
+
   end
 end
