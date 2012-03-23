54dc79dc1df2771640cc98f915a8174d6fb93810
Andrés Pereira <apereira@alabs.es>
2012-03-23 09:08:13 +0100

    offer: refactorizando crawl link/screenchot

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -26,31 +26,41 @@ class Offer < ActiveRecord::Base
     require 'screenchot'
 
     logger.debug "Processing link information ..."
+    crawl_link offer
     # process information
-    if offer.link.include?("www.infojobs.net")
-      logger.debug "Oh! It's infojobs! We're going to crawl the fucker!"
-      result = process_offer("infojobs", offer.link)
-      # FIXME: this is stupid. We should do an array and each all this shit
-      offer.title = result["title"]
-      offer.description = result["description"]
-      offer.studies = result["studies"]
-      offer.province = result["province"]
-      offer.experience = result["experience"]
-      offer.requisites_min = result["requisites_min"]
-      offer.requisites_des = result["requisites_des"]
-      offer.contract_type = result["contract_type"]
-      offer.contract_duration = result["contract_duration"]
-      offer.contract_hour = result["contract_hour"]
-      offer.salary = result["salary"]
-    end
 
     logger.debug "Processing link screenshot ..."
     # process the screenshot
-    title = Time.now.to_i.to_s
-    filename = "/tmp/" + title.gsub(/\s+/, "") + ".png"
-    screenchot(link, filename)
-    offer.screenshot = File.new(filename)
+    crawl_screenchot offer
 
   end
+
+  private
+    
+    def crawl_link(offer)
+      if offer.link.include?("www.infojobs.net")
+        logger.debug "Oh! It's infojobs! We're going to crawl the fucker!"
+        result = process_offer("infojobs", offer.link)
+        # FIXME: this is stupid. We should do an array and each all this shit
+        offer.title = result["title"]
+        offer.description = result["description"]
+        offer.studies = result["studies"]
+        offer.province = result["province"]
+        offer.experience = result["experience"]
+        offer.requisites_min = result["requisites_min"]
+        offer.requisites_des = result["requisites_des"]
+        offer.contract_type = result["contract_type"]
+        offer.contract_duration = result["contract_duration"]
+        offer.contract_hour = result["contract_hour"]
+        offer.salary = result["salary"]
+      end
+    end
+
+    def crawl_screenchot(offer)
+      title = Time.now.to_i.to_s
+      filename = "/tmp/" + title.gsub(/\s+/, "") + ".png"
+      screenchot(offer.link, filename)
+      offer.screenshot = File.new(filename)
+    end
 
 end
