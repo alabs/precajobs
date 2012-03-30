00376732582a06cab0c414e6f89e686b494331d9
Andrés Pereira <apereira@alabs.es>
2012-03-30 12:04:07 +0200

    unicorn debug

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,7 +7,7 @@ worker_processes 4
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-#listen "/tmp/precajobs.socket", :backlog => 64
+listen "/tmp/precajobs.socket", :backlog => 64
 
 # Preload our app for more speed
 preload_app true
